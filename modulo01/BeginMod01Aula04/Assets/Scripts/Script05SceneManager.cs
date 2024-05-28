79955cf2d6d9488fe950e4c6dee06eb8dc1c1314
andrebronca <bronca.andre@gmail.com>
2024-05-28 17:59:55 -0300

    Iniciante - modulo 6 - aula 2

---
+++ modulo01/BeginMod01Aula04/Assets/Scripts/Script05SceneManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;  //sempre que for trabalhar com gerenciamento de cena
+
+
+public class Script05SceneManager : MonoBehaviour
+{
+    void LoadScene()
+    {
+        //vai carregar a cena que tem o nome: Scene01. Não deve ter cenas com nomes repetidos.
+        SceneManager.LoadScene("Scene01");
+        //carrega a cena conforme o index.
+        //File > Build Settings. arrastar a cena do Project para Scenes in Build, irá mostrar o index da cena.
+        SceneManager.LoadScene(1);
+        //Unity carrega a cena enquanto reliza outras tarefas.
+        SceneManager.LoadSceneAsync(1);
+        //Se tiver duas cenas, passando da 1 para a 2, irá remover a 1 e já mostrar a 2.
+        SceneManager.LoadScene("Scene01", LoadSceneMode.Single);
+        //Não remove uma das cenas e carrega a próxima. Permite carregar e descarregar cenas para liberar memória.
+        //exemplo: Game Celeste
+        SceneManager.LoadScene("Scene01", LoadSceneMode.Additive);
+    }
+    
+    public void LoadSceneWithParameterStr(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void LoadSceneWithParameterInt(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- modulo06/README.md
+++ modulo06/README.md
@@ -1,1 +1,20 @@
-# Gerenciamento de cenas e inputs
+# Módulo 6 - Gerenciamento de cenas e Inputs
+
+Revisão: **28/05/2024**<br>
+Instrutor: **Bruno Freitas**<br>
+
+## Aula 1
+- **Cenas**
+    - É +- como uma fase, engloba os gameObjects específicos em determinada cena. É possível ter mais de uma cela ao mesmo tempo no jogo. É possível editar mais de uma cena ao mesmo tempo no jogo.
+    - Não há como não ter uma cena no Unity.
+
+## Aula 2
+- **Gerenciar cena a partir de código**
+    - criando duas cenas
+    - criando um script que irá carregar uma cena
+    - vincular o script em um button que está na cena 1
+        - a função carrega a cena 2
+    - na cena 2 há um cubo vermelho, só para perceber a mudança
+    --**Obs.**: tem que carregar a cena no Build Settings
+
+## Aula 3
